f67a0f82fc107bb6427f892d045563bd49c2c83d
Ilya <ilya.polyakov92@mail.ru>
2022-11-18 13:34:42 +0300

    added task solution

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array1 = new string[6] {"123", "hi", "hello", "world", "2", "Russia"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void SecondArrayItog(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayItog(array1, array2);
+PrintArray(array2);
